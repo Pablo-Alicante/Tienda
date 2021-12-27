0847b8801671474ff47e4fd87a0baa4f6dde3058
Pablo-Alicante <penalverpablo@gmail.com>
2021-12-27 19:10:43 +0100

    Estadio cero del proyecto
    Estadio cero del proyecto

---
+++ MvcSoporte/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcSoporte.Data;
+using MvcSoporte.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSoporte.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MvcSoporteContexto _context;
+
+        public HomeController(ILogger<HomeController> logger, MvcSoporteContexto context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Busca empleado correspondiente al usuario actual. Si existe
+            // va a View y en caso contrario, va a crear el empleado.
+            string emailUsuario = User.Identity.Name;
+            Empleado empleado = _context.Empleados.Where(e => e.Email == emailUsuario)
+            .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            empleado == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
