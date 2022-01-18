7d9de7a522982a794fbe48a53e8d64f4177569ac
Pablo-Alicante <penalverpablo@gmail.com>
2022-01-18 22:17:24 +0100

    Avanzo con el carrito

--- ProyectoIntegrador/ProyectoIntegrador/Controllers/CarritoController.cs
+++ ProyectoIntegrador/ProyectoIntegrador/Controllers/CarritoController.cs
@@ -1,16 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProyectoIntegrador.Data;
+using ProyectoIntegrador.Models;
 
 namespace ProyectoIntegrador.Controllers
 {
     public class CarritoController : Controller
     {
-        public IActionResult Index()
+        private readonly MvcTiendaContexto _context;
+
+        public CarritoController(MvcTiendaContexto context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Carrito
+        public async Task<IActionResult> Index()
+        {
+            string emailUsuario = User.Identity.Name;
+            int ClienteId;
+            Pedido pedidoActual = null;
+            var pendiente = await _context.Estados
+                .Where(e => e.Descripcion == "Pendiente")
+                .FirstOrDefaultAsync();
+
+            var cliente = await _context.Clientes
+                .Where(e => e.Email == emailUsuario)
+                .FirstOrDefaultAsync();
+
+            if (cliente != null)
+            {
+                ClienteId = cliente.Id;
+
+                pedidoActual = await _context.Pedidos
+                    .Include(p => p.Detalles)
+                    .Include(p => p.Cliente)
+                    .Where(e => e.ClienteId == ClienteId && e.EstadoId == pendiente.Id)
+                    .FirstOrDefaultAsync();
+            }
+
+            return View(pedidoActual);
         }
     }
 }
--- ProyectoIntegrador/ProyectoIntegrador/Controllers/EscaparateController.cs
+++ ProyectoIntegrador/ProyectoIntegrador/Controllers/EscaparateController.cs
@@ -111,10 +111,88 @@ namespace ProyectoIntegrador.Controllers
 
             }
 
-
             return Ok();
         }
 
+        // POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Dummy(int? id)
+        {
+            string emailUsuario = User.Identity.Name;
+            int ClienteId;
+            Pedido pedidoActual;
+            var pendiente = await _context.Estados
+                .Where(e => e.Descripcion == "Pendiente")
+                .FirstOrDefaultAsync();
+
+            var cliente = await _context.Clientes
+                .Where(e => e.Email == emailUsuario)
+                .FirstOrDefaultAsync();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            if (cliente != null)
+            {
+                ClienteId = cliente.Id;
+
+                pedidoActual = await _context.Pedidos
+                    .Include(p => p.Detalles)
+                    .Include(p => p.Cliente)
+                    .Where(e => e.ClienteId == ClienteId && e.EstadoId == pendiente.Id)
+                    .FirstOrDefaultAsync();
+
+                if (pedidoActual != null)
+                {
+                    // si hemos encontrado el pedido, añadimos una nueva línea al pedido.
+                    Detalle detalle = new Detalle();
+                    detalle.ProductoId = producto.Id;
+                    detalle.Cantidad = 1;
+                    detalle.Descuento = 0;
+                    detalle.Precio = producto.Precio;
+                    detalle.PedidoId = pedidoActual.Id;
+
+                    pedidoActual.Detalles.Add(detalle);
+
+                    _context.Detalles.Add(detalle);
+                }
+                else
+                {
+                    // en caso contrario, hay que crear el pedido y que este sea la primera línea.
+                    pedidoActual = new Pedido();
+                    pedidoActual.ClienteId = 1;
+                    pedidoActual.Detalles = new List<Detalle>();
+                    pedidoActual.EstadoId = 1;
+                    pedidoActual.Fecha = DateTime.Now;
+
+                    Detalle detalle = new Detalle();
+                    detalle.ProductoId = producto.Id;
+                    detalle.Cantidad = 1;
+                    detalle.Descuento = 0;
+                    detalle.Precio = producto.Precio;
+                    detalle.PedidoId = pedidoActual.Id;
+
+                    pedidoActual.Detalles.Add(detalle);
+
+                    _context.Pedidos.Add(pedidoActual);
+                }
+
+            }
+
+            return Ok();
+        }
 
     }
 }
