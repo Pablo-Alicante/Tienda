7ed6c38a2523b0bb233ef0f2c609c25f288c700a
Pablo-Alicante <penalverpablo@gmail.com>
2022-01-24 19:08:52 +0100

    CarritoController HECHO
    Paso las fotos de CarritoController hecho del código del profesor. Las hago comentando el código porque no estoy seguro de si coincide con lo que ya llevo hecho.

--- ProyectoIntegrador/ProyectoIntegrador/Controllers/CarritoController.cs
+++ ProyectoIntegrador/ProyectoIntegrador/Controllers/CarritoController.cs
@@ -22,6 +22,154 @@ namespace ProyectoIntegrador.Controllers
         // GET: Carrito
         public async Task<IActionResult> Index()
         {
+            // int intNumeroPedido = 0;
+            // string numeroPedido = httpContext.Session.GetString("NumPedido");
+            // if (numeroPedido == null)
+            // {
+            // return view("CarritoVacio"); 
+            // }
+            // intNumeroPedido = Convert.ToInt32(numeroPedido);
+            // var pedido = await_context.Pedidos
+            // .Include(x =>x.Cliente)
+            // .Include(x =>x.Estado)
+            // .Include(x=>x.Detalles)
+            // .Include(x =>x.Producto)
+            // .ThenInclude(x=>x.Producto)+.FirtsOrDefaultAsync(e => e.Id == intNumeroPedido);
+
+            // if(pedido == null)
+            // {
+            // return NotFound();
+            // }
+            // return View(pedido);
+            // }
+
+            //GET Carrito vacío
+            // private bool DetalleExists(int id)
+            // {
+            // retunr _context.Detalles.Any(p =>p.Id == id);
+            // }
+
+            // public async Task<ActionResult>MasCantidad(int ? id)
+            // {
+            // if(id == null)
+            // {
+            // return NotFound();
+            // }
+
+            // var detalle = await_context.Detalles.FindAsync(id);
+            // detalle.Cantidad = detalle.Cantidad + 1;
+
+            // if(ModelState.IsValid)
+            // {
+            // try
+            // {
+            // _context.Update(detalle);
+            // await_context.SaveChangesAsync();
+            // }
+
+            // catch(DbUpdateConcurrencyException)
+            // {
+            // if(!DetalleExists(detalle.id))
+            // {
+            // return NotFound();
+            // }
+            // else
+            // {
+            // throw;
+            // }
+            // }
+            // }
+            // return RedirectToAction(nameof(Index));
+            // }
+
+
+            // GET carrito/MenosCantidad
+
+            // public async Task<ActionResult>MenosCantidad(int ? id)
+            // {
+            // if(id == null)
+            // {
+            // return NotFound();
+            // }
+
+            // var detalle = await_context.Detalles.FindAsync(id);
+            // detalle.Cantidad = detalle.Cantidad - 1;
+            // if(ModelState.IsValid)
+            // {
+            // try
+            // {
+            // _context.Update(detalle);
+            // await_context.SaveChangeAsync();
+            // }
+            // catch(DbUpdateConcurrencyException)
+            // {
+            // if(!DetalleExisists(detalle,id))
+            // {
+            // return NotFound();
+            // }
+            // else
+            // {
+            // throw;
+            // }
+            // }
+            // }
+            // return RedirectToAction(nameof(Index));
+            // }
+
+            // POST Carrito/ConfirmarPedido
+
+            // [HttpPost]
+            // [ValidateAntiForgeryToken]
+            // public async Task<IActionResult> ConfirmarPedido(int ? id)
+            // {
+            // if(id == null)
+            // {
+            // return NotFound();
+            // }
+
+            // var pedido = await_context.Pedidos.FindAsync(id);
+
+
+            // se cambia el estado del pedido a confirmado
+
+            // pedido.EstadoId = 2;
+            // pedido.Confirmado = DateTime.Now;
+
+            // if(ModelState.IsValid)
+            // {
+            // try
+            // {
+            // _context.Update(pedido);
+            // await_context.SaveChangesAsync();
+            // Al confirmar el pedido se pone la variable de sesion del pedido actual
+            // HttpContext.Session.Remove("NumPedido");
+            // }
+            // catch(DbUpdateConcurrencyException)
+            // {
+            // if(!PedidoExists(pedido.id)
+            // {
+            // return NotFound();
+            // }
+            // else
+            // {
+            // throw;
+            // }
+            // }
+            // }
+            // return RedirectToAction(nameof(Index), "Escaparate");
+            // }
+
+            // private bool PedidoExists(int id)
+            // {
+            // return _context.Pedidos.Any(p=>p.Id ==id);
+            // }
+            // }
+            // }
+
+
+
+
+
             string emailUsuario = User.Identity.Name;
             int ClienteId;
             Pedido pedidoActual = null;
